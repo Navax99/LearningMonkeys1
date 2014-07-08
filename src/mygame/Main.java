fa4ee0088c5b4d14252bdbd7fee1126d554d0b41
Albert <Albert@Albert-PC>
2014-07-08 15:01:06 +0200

    afegit canvi de color per temps

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -3,6 +3,7 @@ package mygame;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
@@ -10,34 +11,52 @@ import com.jme3.scene.shape.Box;
 
 /**
  * test
+ *
  * @author normenhansen
  */
 public class Main extends SimpleApplication {
-
+    
+    private myTimer mytimer = new myTimer(getTimer());
+    private BoxField boxfield;
+    
     public static void main(String[] args) {
         Main app = new Main();
         app.start();
     }
-
+    
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        boxfield = new BoxField(rootNode, assetManager, 10, 10, 10);
+        boxfield.setRandomBox();
+        runTimer();
+        setUpCam();
+        
     }
-
+    
     @Override
     public void simpleUpdate(float tpf) {
         //TODO: add update code
+        if (mytimer.isfinish()) {
+            boxfield.setRandomBox();
+            runTimer();
+        }
+        
     }
-
+    
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
+    }
+    
+    private void runTimer() {
+        mytimer.setTimer(0, 10);
+    }
+    
+    private void setUpCam(){
+        cam.setLocation(new Vector3f(-2, 2, -2));
+        cam.setRotation( cam.getRotation().fromAngleAxis(50* FastMath.DEG_TO_RAD, Vector3f.UNIT_Y));
+        //regenerem la camara perspectiva
+        cam.setFrustumPerspective(45f, (float) cam.getWidth() / cam.getHeight(), 0.01f, 1000f);
+        flyCam.setMoveSpeed(4);
     }
 }
